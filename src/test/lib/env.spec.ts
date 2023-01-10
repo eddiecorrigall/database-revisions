2ae76d1a55e2ff2b09d79bcae84f4df3a59edf13
Edward Corrigall <eddiecorrigall@gmail.com>
2023-01-10 07:30:06 -0500

    fix: test actual vs expected order

--- src/test/lib/env.spec.ts
+++ src/test/lib/env.spec.ts
@@ -5,7 +5,7 @@ import { expectEnv } from '../../lib/env'
 describe('expectEnv', () => {
   it('should return the value of an existing environment variable', () => {
     process.env.MY_ENV_VAR = 'abc123'
-    assert.equal('abc123', expectEnv('MY_ENV_VAR'))
+    assert.equal(expectEnv('MY_ENV_VAR'), 'abc123')
   })
   it('should throw an error if the environment variable does not exist', () => {
     delete process.env.MY_ENV_VAR
