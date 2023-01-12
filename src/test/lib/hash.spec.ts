ccdccb5e58c3e4ccb55a77249d4a8c164eeb660d
Edward Corrigall <eddiecorrigall@gmail.com>
2023-01-12 11:24:06 -0500

    fix: hash tests

--- src/test/lib/hash.spec.ts
+++ src/test/lib/hash.spec.ts
@@ -1,12 +1,24 @@
 import assert from 'assert'
 
-import { hash } from '../../lib/hash'
+import { hash, HashAlgorithm } from '../../lib/hash'
 
 describe('hash', () => {
-  it('should return sha256 digest of string content by default', () => {
-    assert.equal(
-      hash('Hello world!'),
-      'c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a'
-    )
+  describe('when content is string', () => {
+    describe('when algorithm is sha256', () => {
+      it('should return sha256 digest of string content', () => {
+        assert.equal(
+          hash('Hello world!', HashAlgorithm.SHA256),
+          'c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a'
+        )
+      })
+    })
+    describe('when algorithm is not provided', () => {
+      it('should return sha1 digest of string content', () => {
+        assert.equal(
+          hash('Hello world!'),
+          'd3486ae9136e7856bc42212385ea797094475802'
+        )
+      })
+    })
   })
 })
